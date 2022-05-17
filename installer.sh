9929fc0460e169cd2a464649d5d429fdece5d6d5
racknerd <gostnq@gmail.com>
2022-05-17 04:38:17 +0100

    autoinstall zsh-plugins

--- installer.sh
+++ installer.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
@@ -9,5 +9,16 @@ ln -sf ${BASEDIR}/vim ~/.vim
 # zsh
 ln -sf ${BASEDIR}/zshrc ~/.zshrc
 
+if [! -d $ZSH_CUMSTOM/plugins/zsh-autosuggestions ];then
+	echo "installing zsh-autosuggestions..."
+	env -i git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
+
+if [ ! -d "$ZSH_CUMSTOM/plugins/zsh-syntax-highlighting" ];then
+	echo "installing zsh-syntax-highlighting..."
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+fi
+
 # tmux
 ln -sf ${BASEDIR}/tmux.conf ~/.tmux.conf
+
