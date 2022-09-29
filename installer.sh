827cc910e6f218a9221f70b86df057b2ad9ff501
qwer <Davego@protonmail.com>
2022-09-30 01:01:55 +0800

    repair zsh plugins install bugs

--- installer.sh
+++ installer.sh
@@ -9,13 +9,15 @@ ln -sf ${BASEDIR}/vim ~/.vim
 # zsh
 ln -sf ${BASEDIR}/zshrc ~/.zshrc
 
-if [ -e $ZSH_CUMSTOM/plugins/zsh-autosuggestions ];then
-	echo "installing zsh-autosuggestions..." && git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-fi
+# if [ -e ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ];then
+	echo "installing zsh-autosuggestions..." # && 
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# fi
 
-if [ -e "$ZSH_CUMSTOM/plugins/zsh-syntax-highlighting" ];then
-	echo "installing zsh-syntax-highlighting..." && git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-fi
+# if [ -e ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting ];then
+	echo "installing zsh-syntax-highlighting..." # && 
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# fi
 
 # tmux
 ln -sf ${BASEDIR}/tmux.conf ~/.tmux.conf
